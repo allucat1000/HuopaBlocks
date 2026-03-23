5ecc4469a34df6bdd3b883f81fb6600b869dec63
Allucat1000 <79474282+allucat1000@users.noreply.github.com>
2026-03-23 22:12:29 +0100

    notif

--- src/svelte/App.svelte
+++ src/svelte/App.svelte
@@ -5,6 +5,26 @@
   import Sidebar from "./Sidebar.svelte";
   import Topbar from "./Topbar.svelte";
   import { palette } from "../ts/palette";
+  import { mount, onMount, unmount } from "svelte";
+  import Toast from "./Toast.svelte";
+
+  onMount(() => {
+    const popupViewed = localStorage.getItem("HuopaBlocksBrowserPopupViewed");
+    const unspBrws = !("userAgentData" in navigator);
+    const c = document.querySelector(".CodeArea");
+    if (unspBrws && c && !popupViewed) {
+      localStorage.setItem("HuopaBlocksBrowserPopupViewed", "true");
+      const t = mount(Toast, {
+        target: c,
+        props: {
+          title: "Possible issues",
+          text: "Your browser does not support certain features, which may cause some issues with styles. Use a Chromium-based browser for full support.",
+          duration: 3000,
+          close: () => unmount(t)
+        }
+      });
+    }
+  })
 
   initTheme();
 </script>
---
+++ src/svelte/Toast.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    const { title, text, duration, close }: { title: string, text: string, duration: number, close: Function } = $props();
+    let main: HTMLDivElement;
+    setTimeout(() => {
+        main.classList.add("ToastClosing");
+        setTimeout(close, 1000);
+    },
+    // svelte-ignore state_referenced_locally
+    duration);
+</script>
+<div class="Toast" bind:this={main}>
+    <p class="ToastTitle">{title}</p>
+    <p class="ToastDescription">{text}</p>
+</div>
