46cec9da9213a00395779c97ce4fd6f54533e0ae
Allucat1000 <79474282+allucat1000@users.noreply.github.com>
2026-03-23 21:44:13 +0100

    project saving, some improvements all throughout

--- src/svelte/Topbar.svelte
+++ src/svelte/Topbar.svelte
@@ -6,6 +6,7 @@
     import { CurrentTheme } from "../ts/main.svelte";
     import Popup from "./Popup.svelte";
     import { PopupElementTag } from "../ts/types";
+    import TopbarDropdown from "./TopbarDropdown.svelte";
     let popupOpen: boolean = false;
     function viewJSON() {
         if (popupOpen) return;
@@ -30,6 +31,24 @@
             }
         });
     }
+
+    function saveProject() {
+        const a = document.createElement("a");
+
+        const data = JSON.stringify(project, null, 2);
+        const blob = new Blob([data], { type: "application/json" });
+
+        const url = URL.createObjectURL(blob);
+
+        a.href = url;
+        a.download = `${project.name || "project"}`;
+
+        document.body.appendChild(a);
+        a.click();
+
+        document.body.removeChild(a);
+        URL.revokeObjectURL(url);
+    }
 </script>
 
 <div class="Topbar">
@@ -39,6 +58,9 @@
     {:else}
         <LightIcon class="ThemeToggle" onclick={() => CurrentTheme.dark = !CurrentTheme.dark} />
     {/if}
+    <TopbarDropdown title="File">
+        <button class="TopbarDropdownOption" onclick={saveProject}>Save Project</button>
+    </TopbarDropdown>
     <button class="TopbarButton" onclick={viewJSON}>View project JSON</button>
     <input class="TopbarInput" placeholder="Give this project a name!" bind:value={project.name}>
 </div>
---
+++ src/svelte/TopbarDropdown.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import type { Snippet } from "svelte";
+
+    const { title, children }: { title: string, children: Snippet } = $props(); 
+
+    let visible: boolean = $state(false);
+</script>
+<div class="TopbarDropdown">
+    <button onclick={() => visible = !visible} class="TopbarButton" {title}>{title}</button>
+    <div class={`TopbarDropdownOptions ${visible ? "" : "Hidden"}`}>
+        {@render children()}
+    </div>
+</div>
