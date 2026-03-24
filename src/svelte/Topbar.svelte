6bb681eb4439bd651ba8d1068b9d7477e0fb1679
Allucat1000 <79474282+allucat1000@users.noreply.github.com>
2026-03-24 20:23:48 +0200

    json view now closes dropdown

--- src/svelte/Topbar.svelte
+++ src/svelte/Topbar.svelte
@@ -14,6 +14,7 @@
 
     function viewJSON() {
         if (popupOpen) return;
+        projectDropdownVisiblity = false;
         popupOpen = true;
         const p = mount(Popup, {
             target: document.querySelector("#main")!,
