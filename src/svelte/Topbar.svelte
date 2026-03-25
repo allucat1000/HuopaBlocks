4400507484e7bc8020a2a741f9bcdeb78ce36dfa
Allucat1000 <79474282+allucat1000@users.noreply.github.com>
2026-03-25 14:25:50 +0200

    project load prompt now 'only' accepts .zip and .hblock

--- src/svelte/Topbar.svelte
+++ src/svelte/Topbar.svelte
@@ -84,7 +84,7 @@
     <TopbarDropdown title="Project" bind:visible={projectDropdownVisiblity}>
         <button class="TopbarDropdownOption" title="Save Project" onclick={saveProject}>Save Project</button>
         <div class="TopbarDropdownOption TopbarDropdownInputOption">
-            <input class="ProjectLoadInput" onchange={loadProject} type="file" title="Load Project"><span>Load Project</span>
+            <input class="ProjectLoadInput" onchange={loadProject} type="file" title="Load Project" accept=".zip, .hblock"><span>Load Project</span>
         </div>
         <button class="TopbarDropdownOption" title="View JSON Data" onclick={viewJSON}>View JSON Data</button>
     </TopbarDropdown>
